53b908a5bc71bb48edb8fb7b6c40c32a947ec669
Ga-yo <39812@naver.com>
2020-11-21 00:31:57 +0900

    Feat : portfolio tableviewcell

---
+++ PiPi/PiPi/Source/Util/PortfolioTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PortfolioTableViewCell.swift
+//  PiPi
+//
+//  Created by 이가영 on 2020/11/19.
+//
+
+import UIKit
+
+class PortfolioTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var projectNameLabel: UILabel!
+    @IBOutlet weak var giturlLabel: UILabel!
+    @IBOutlet weak var moreLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
