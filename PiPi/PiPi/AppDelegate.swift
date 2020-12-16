7376890ccf17c8c6cdf3afe2b43fe44fa943d8bb
Ga-yo <39812@naver.com>
2020-12-16 16:09:26 +0900

    Feat : not allow dark mode

--- PiPi/PiPi/AppDelegate.swift
+++ PiPi/PiPi/AppDelegate.swift
@@ -13,7 +13,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        if #available(iOS 13.0, *) {
+          self.window?.overrideUserInterfaceStyle = .light
+        }
+        
         return true
     }
 }
